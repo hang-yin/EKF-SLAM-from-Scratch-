8d7e7ec593e3d09a4e5c3139ccfba30eb57d3609
hang-yin <yinhang0226@gmail.com>
2023-01-13 14:12:45 -0600

    Task B.1 done

--- turtlelib/rigid2d.cpp
+++ turtlelib/rigid2d.cpp
@@ -4,24 +4,62 @@
 #include <iostream>
 #include <cmath>
 
-namespace rigid2d
+namespace turtlelib
 {
-    // constructor
-    Transform2D::Transform2D()
+
+    std::ostream & operator<<(std::ostream & os, const Vector2D & v)
     {
-        // initialize to identity matrix
-        T[0][0] = 1.0;
-        T[0][1] = 0.0;
-        T[0][2] = 0.0;
-        T[1][0] = 0.0;
-        T[1][1] = 1.0;
-        T[1][2] = 0.0;
-        T[2][0] = 0.0;
-        T[2][1] = 0.0;
-        T[2][2] = 1.0;
+        os << "[" << v.x << " " << v.y << "]";
+        return os;
     }
 
-    // constructor
-    Transform2D::Transform2D(double
-    theta,
-    Vector
+    std::istream & operator>>(std::istream & is, Vector2D & v)
+    {
+        char first_char = is.peek();
+        if (first_char == '[')
+        {
+            is.get();
+            is >> v.x >> v.y;
+        }
+        else
+        {
+            is >> v.x >> v.y;
+        }
+        return is;
+    }
+
+    std::ostream & operator<<(std::ostream & os, const Transform2D & tf)
+    {
+        os << "deg: " << rad2deg(tf.rotation()) << " x: " << tf.translation().x << " y: " << tf.translation().y;
+        return os;
+    }
+
+    std::istream & operator>>(std::istream & is, Transform2D & tf)
+    {
+        char first_char = is.peek();
+        char temp[10];
+        Vector2D temp_v;
+        double temp_theta;
+        if (first_char =='d'){
+            is.get(temp, 6);
+            is >> temp_theta;
+            temp_theta = deg2rad(temp_theta);
+            is.get(temp, 4);
+            is >> temp_v.x;
+            is.get(temp, 4);
+            is >> temp_v.y;
+            tf = Transform2D(temp_v, temp_theta);
+        }
+        else{
+            is >> temp_theta >> temp_v.x >> temp_v.y;
+            tf = Transform2D(temp_v, deg2rad(temp_theta));
+        }
+        return is;
+    }
+
+    Transform2D operator*(Transform2D lhs, const Transform2D & rhs)
+    {
+        return lhs *= rhs;
+    }
+
+}
--- turtlelib/rigid2d.hpp
+++ turtlelib/rigid2d.hpp
@@ -22,6 +22,7 @@ namespace turtlelib
     /// if given a compile-time constant as input
     constexpr bool almost_equal(double d1, double d2, double epsilon=1.0e-12)
     {
+        return std::abs(d1 - d2) < epsilon;
     }
 
     /// \brief convert degrees to radians
@@ -29,6 +30,7 @@ namespace turtlelib
     /// \returns radians
     constexpr double deg2rad(double deg)
     {
+        return deg * PI / 180.0;
     }
 
     /// \brief convert radians to degrees
@@ -36,6 +38,7 @@ namespace turtlelib
     /// \returns the angle in degrees
     constexpr double rad2deg(double rad)
     {
+        return rad * 180.0 / PI;
     }
 
     /// static_assertions test compile time assumptions.
