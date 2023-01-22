70e958a892757c6c70b4ebe949e9ba585ef4f945
hang-yin <yinhang0226@gmail.com>
2023-01-22 11:49:54 -0600

    working on teleport service

--- nusim/src/nusim.cpp
+++ nusim/src/nusim.cpp
@@ -1,6 +1,10 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/u_int64.hpp"
 #include "std_srvs/srv/empty.hpp"
+#include "tf2_ros/transform_broadcaster.h"
+#include "tf2/LinearMath/Quaternion.h"
+#include "geometry_msgs/msg/transform_stamped.hpp"
+#include "srv/Teleport.srv"
 #include <chrono>
 #include <functional>
 #include <memory>
@@ -11,7 +15,7 @@ using namespace std::chrono_literals;
 class NuSimNode : public rclcpp::Node
 {
 public:
-  NuSimNode(double rate) : Node("nusim")
+  NuSimNode(double rate, float x0, float y0, float theta0) : Node("nusim")
   {
 	// Initialize timestep to zero
 	timestep_ = 0;
@@ -39,7 +43,33 @@ public:
 		(void)request;
 		// Reset timestep to zero
 		timestep_ = 0;
+		// Reset transform
+		transform_stamped_.transform.translation.x = x0_;
+		transform_stamped_.transform.translation.y = y0_;
+		transform_stamped_.transform.rotation.x = q0_.x();
+		transform_stamped_.transform.rotation.y = q0_.y();
+		transform_stamped_.transform.rotation.z = q0_.z();
+		transform_stamped_.transform.rotation.w = q0_.w();
 	  });
+	
+	// Set initial pose
+	x0_ = x0;
+	y0_ = y0;
+	theta0_ = theta0;
+	q0_.setRPY(0, 0, theta0_);
+
+	// Create transform broadcaster from the frame "nusim/world" to the frame "red/base_footprint"
+	// tf2_ros::TransformBroadcaster broadcaster(this);
+	// geometry_msgs::msg::TransformStamped transformStamped;
+	transformStamped_.header.frame_id = "nusim/world";
+	transformStamped_.child_frame_id = "red/base_footprint";
+	transformStamped_.transform.translation.x = x0_;
+	transformStamped_.transform.translation.y = y0_;
+	transformStamped_.transform.translation.z = 0.0;
+	transformStamped_.transform.rotation.x = q0_.x();
+	transformStamped_.transform.rotation.y = q0_.y();
+	transformStamped_.transform.rotation.z = q0_.z();
+	transformStamped_.transform.rotation.w = q0_.w();
   }
 
 private:
@@ -49,21 +79,31 @@ private:
 		auto msg = std_msgs::msg::UInt64();
 		msg.data = timestep_;
 		timestep_pub_->publish(msg);
+		// Broadcast transform
+		transformStamped.header.stamp = this->now();
+		broadcaster_.sendTransform(transformStamped_);
 		// Increment timestep
 		timestep_++;
 	}
 	rclcpp::TimerBase::SharedPtr timer_;
 	rclcpp::Publisher<std_msgs::msg::UInt64>::SharedPtr timestep_pub_;
 	rclcpp::Service<std_srvs::srv::Empty>::SharedPtr reset_srv_;
+	tf2_ros::TransformBroadcaster broadcaster_;
+	geometry_msgs::msg::TransformStamped transformStamped_;
 	uint64_t timestep_;
 	double rate_;
+	float x0_;
+	float y0_;
+	float theta0_;
+	tf2::Quaternion q0_;
+
 };
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
   double rate = 0;
-  rclcpp::spin(std::make_shared<NuSimNode>(rate));
+  rclcpp::spin(std::make_shared<NuSimNode>(rate, -0.6, 0.8, 1.57));
   rclcpp::shutdown();
   return 0;
 }
